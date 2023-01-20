803cb016c69267f33b0a0248dd8b803af75f12fd
Robert Xing <robertxing2004@gmail.com>
2023-01-19 22:55:47 -0500

    about me underway

--- src/App.jsx
+++ src/App.jsx
@@ -46,9 +46,19 @@ function App() {
         </div>
         <div class="about-content">
           <div class="about-text">
-            
+            <p>
+              I'm currently a first year engineering student at Western University<br/>
+              studying software engineering and hoping to pursue the Ivey HBA<br/>
+              dual degree program
+            </p>
+            <p>
+            Aside from school and work, I enjoy travelling all over the world and<br/>
+            taking lots of photos! I also have a huge love for aviation, and I hope<br/>
+            to get my Canadian private pilot's license once I have the time and<br/>
+            money(lol) to do so!
+            </p>
           </div>
-          <div class="about-img">
+          <div>
             <img class="me" src={ me } alt=""/>
           </div>
         </div>
