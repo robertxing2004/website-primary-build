5651ba644a31ac46a82b4475c346e9f15246eb73
Robert Xing <robertxing2004@gmail.com>
2023-09-20 15:10:48 -0400

    feat: contact form done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,6 +10,20 @@ class Contact extends React.Component {
         <div className="contact" id="contact">
           <div className="contact-title">
             <h2>| contact</h2>
+            <form>
+              <label>
+                EMAIL<br/>
+                <input type="text" className="email" name="email" placeholder="name@example.com"/>
+              </label>
+              <br/>
+              <label>
+                MESSAGE<br/>
+                <textarea type="text" className="message" name="message" rows="5" placeholder="what's on your mind?"/>
+              </label>
+              <label>
+                <input type="submit" className="submit" value="SUBMIT"/>
+              </label>
+            </form>
           </div>
         </div>
       </FadeIn>
